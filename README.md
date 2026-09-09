e72e79336b9a153be962b9a06820d51f2d4c8baf
Yhzany95 <Yhzan95@protonmail.com>
2026-09-09 13:39:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Collectible
+Collectible Project
---
+++ Source/Collectible/Collectible.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Collectible.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Collectible, "Collectible" );
+
+DEFINE_LOG_CATEGORY(LogCollectible)
